f89a0bb90b3902e05f40acb869573bb6843b7555
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-05-25 21:05:21 +0400

    Add files via upload

---
+++ 220525 Classwork1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _220525_Classwork
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car car = new Car( -100);
+            Console.WriteLine(car.MaxSpeed);
+        }
+    }
+}
---
+++ 220525 Classwork2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220525_Classwork
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        
+        public readonly int MaxSpeed;
+
+
+        public Car (int maxSpeed)
+        {
+            if (maxSpeed > 0)
+            {
+                MaxSpeed = maxSpeed;
+            }
+            else MaxSpeed = 140;        
+        }   
+    }
+}
